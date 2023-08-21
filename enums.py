efbc1a9edd6bf335fc0639ae0dc970ec823f0a58
Shahboz <sharafzodashahboz@gmail.com>
2023-08-21 11:42:00 +0500

    enums modified

--- enums.py
+++ enums.py
@@ -1,8 +1,9 @@
 from enum import Enum 
 
+
 class DepartmentEnum(Enum):
     ARTS_AND_HUMANITIES = 'Arts and Humanities'
     LIFE_SCIENCES = 'Life Sciences'
     SCIENCE_AND_ENGINEERING = 'Science and Engineering'
-    
+
 
