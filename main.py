395342068cbe44a7c1abba58ec40734b3591a0a0
Shahboz <sharafzodashahboz@gmail.com>
2023-08-23 11:20:47 +0500

    some more info about pydantic

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import requests 
-from pprint import pprint
-from schemas import Student
+from models import Student, Module
+import json 
 
 
-url = 'https://raw.githubusercontent.com/bugbytes-io/datasets/master/students_v1.json'
+url = 'https://raw.githubusercontent.com/bugbytes-io/datasets/master/students_v2.json'
 
 response = requests.get(url)
 data = response.json()
+
+# nested models pydantic 6:50
 
 # we can append dummy data to ensure that our custom validation is working properly
 
@@ -22,10 +24,54 @@ data = response.json()
 #     }
 # )
 
+# since we appended the item which credit value is 24 we are getting validation error
+# after module length validation when we try to append one more elemen
+# data[-1]['modules'].append(
+#     {
+#         "id": 101,
+#         "name": "Japanese Cinema",
+#         "professor": "Prof. Travis Hudson",
+#         "credits": 20,
+#         "registration_code": "abc"
+#     }
+# )
 
-for student in data:
-    model = Student(**student)
-    pprint((model.model_dump_json()))
+
+
+# for student in data:
+#     model = Student(**student)
+# # now we can iterate over the module. Remember it is the field of Student model
+#     # for module in model.modules:
+#     #     # since the id field is the union of id and uuid we get both and no error
+#     #     print(module.id)
+#     print(model)
+
+
+# Lets look at something called json schema. It is a declarative language that allows
+# you to annotate and validate JSON documents 
+# Now we can learn how to take Pydantic model and convert it to JSON Schema
+
+# print(Module.schema_json(indent=2))
+# print(json.dumps(Module.model_json_schema(),indent=2))
+print(json.dumps(Student.model_json_schema(),indent=2))
+
+# now we can do something reverse, for instance we are given a json file and we want to
+# convert it back to Pydantic model. For that we can use a library called
+# datamodel-code-generator
+
+# this library provides a command line tool called 
+# datamodel-codegen --input (you should input your file) --output models.py
+
+# while generating keep in mind that custom validations will not be taken into consideration
+
+
+
+
+
+
+        
+
+
     
     
 
